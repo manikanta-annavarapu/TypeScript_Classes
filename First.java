6692cb105ba9be5c286f831d351df88da5da2325
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-07-10 23:39:25 +0530

    squaring a number functionality added

--- First.java
+++ First.java
@@ -2,6 +2,10 @@ class First{
     public static void main(String args[]){
         String something = "mani";
         System.out.println(something.length());
-        System.out.println("Hello World");
+        System.out.println(squareNumber(5));
+    }
+
+    public static int squareNumber(int number){
+    	return number * number;
     }
 }
