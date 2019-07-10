5242e5e5fc9202d5a66e4dc88ed000324cde6200
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-07-10 22:58:58 +0530

    uploaded first program

---
+++ First.java
@@ -0,0 +1,7 @@
+class First{
+    public static void main(String args[]){
+        String something = "mani";
+        System.out.println(something.length());
+        System.out.println("Hello World");
+    }
+}
